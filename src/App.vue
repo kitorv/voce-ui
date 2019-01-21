8288adb4cbf19bc864120fa144e7b5d67e7e9946
kitorv <826526881@qq.com>
2019-01-21 20:25:34 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    Welcome to Your Vue.js App
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app"
+};
+</script>
+
+<style lang="scss">
+html,
+body {
+  padding: 0;
+  margin: 0;
+  height: 100%;
+  background: #f2f3f8;
+}
+#app {
+  font-family: "Microsoft YaHei", "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  height: 100%;
+  width: 100%;
+}
+</style>
