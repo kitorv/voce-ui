8d3645b174537fad6664d296bfe9677109ee80e8
kitorv <826526881@qq.com>
2019-01-22 16:48:29 +0800

    动态初始话表格

---
+++ packages/components/datagrid-header.vue
@@ -0,0 +1,69 @@
+<template>
+  <table class="vk-datagrid--header-table" cellspacing="0" cellpadding="0" border="0">
+    <thead>
+      <tr v-for="(colRows, index) in columns" :key="index">
+        <th v-for="(column, index) in colRows" :key="index" :colspan="column.colSpan" :rowspan="column.rowSpan">
+          <div class="vk-datagrid--header-table-cell">{{column.title}}</div>
+        </th>
+      </tr>
+    </thead>
+  </table>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    columns: {
+      required: true,
+      type: Array
+    }
+  },
+  computed: {
+    // 获取父级表格
+    datgrid() {
+      return this.$parent;
+    }
+  },
+  methods: {},
+  mounted() {
+    console.log(this.columns);
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.vk-datagrid--header-table {
+  table-layout: fixed;
+  border-collapse: separate;
+
+  th {
+    padding: 12px 0;
+    min-width: 0;
+    box-sizing: border-box;
+    vertical-align: middle;
+    position: relative;
+    text-align: left;
+    border: 1px solid #ebeef5;
+    white-space: nowrap;
+    overflow: hidden;
+    user-select: none;
+  }
+}
+
+.vk-datagrid--header-table-cell {
+  position: relative;
+  word-wrap: normal;
+  vertical-align: middle;
+  width: 100%;
+  display: inline-block;
+  box-sizing: border-box;
+  text-overflow: ellipsis;
+  white-space: normal;
+  word-break: break-all;
+  line-height: 20px;
+}
+</style>
+
--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,48 @@
 <template>
   <div id="app">
-    Welcome to Your Vue.js App
+    <v-data-grid :columns="columns"></v-data-grid>
   </div>
 </template>
 
 <script>
+import VDataGrid from "../packages/components/datagrid";
+
 export default {
-  name: "app"
+  name: "app",
+  components: { VDataGrid },
+  data() {
+    return {
+      columns: [
+        { key: "id", title: "id标识" },
+        {
+          title: "用户信息",
+          children: [
+            {
+              title: "基本信息",
+              children: [
+                { key: "name", title: "姓名" },
+                { key: "age", title: "年龄" },
+                { key: "work", title: "工作" }
+              ]
+            }
+          ]
+        },
+        {
+          title: "其他信息",
+          children: [
+            { key: "street", title: "街道" },
+            {
+              title: "详细地址",
+              children: [
+                { key: "building", title: "位置" },
+                { key: "doorNo", title: "门牌号" }
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
 };
 </script>
 
@@ -16,7 +52,6 @@ body {
   padding: 0;
   margin: 0;
   height: 100%;
-  background: #f2f3f8;
 }
 #app {
   font-family: "Microsoft YaHei", "Avenir", Helvetica, Arial, sans-serif;
