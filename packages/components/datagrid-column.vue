a3c68840450d757f75c77601b88a77f4b6a9eecb
kitorv <826526881@qq.com>
2019-02-06 01:19:19 +0800

    动态jsx列渲染

---
+++ packages/components/datagrid-column.vue
@@ -0,0 +1,35 @@
+<script>
+import DCheckbox from "./datagrid-checkbox";
+
+export default {
+  components: { DCheckbox },
+  inject: ["datagrid"],
+  props: {
+    data: { required: true, type: Object }
+  },
+  render(h) {
+    let { title, type } = this.data;
+    if (!type) return <span>{title}</span>;
+
+    switch (type) {
+      case "checkbox":
+        return this.renderChechbox(h, this.data);
+      default:
+        //   TODO动态化配置
+        return <span>{title}</span>;
+    }
+  },
+  methods: {
+    renderChechbox(h, column) {
+      let datagrid = this.datagrid;
+      return (
+        <d-checkbox
+          indeterminate={datagrid.indeterminate}
+          value={datagrid.checked}
+          nativeOn-click={datagrid.checkAll}
+        />
+      );
+    }
+  }
+};
+</script>
