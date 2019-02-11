e290e20d23dbed88ef95db64edbd0c8dd3b2d36e
kitorv <826526881@qq.com>
2019-02-11 23:02:58 +0800

    部分示例

---
+++ src/views/basic.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-datagrid :columns="columns"
+              :data-source="dataSource"></v-datagrid>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      columns: [
+        { title: "Name", key: "name" },
+        { title: "Age", key: "age" },
+        { title: "Address", key: "address" }
+      ],
+      dataSource: [
+        { key: "1", name: "John Brown", age: 32 },
+        { key: "2", name: "Jim Green", age: 42 },
+        { key: "3", name: "Joe Black", age: 32 }
+      ]
+    };
+  }
+};
+</script>
