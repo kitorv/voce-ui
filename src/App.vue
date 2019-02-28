d93a6919e57e9264b95210064788168aa63fabae
kitorv <826526881@qq.com>
2019-02-28 15:08:06 +0800

    表格主体重新构建

--- src/App.vue
+++ src/App.vue
@@ -15,15 +15,11 @@ html,
 body {
   padding: 0;
   margin: 0;
-  height: 100%;
 }
 #app {
   font-family: "Microsoft YaHei", "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  height: 100%;
-  width: 100%;
   box-sizing: border-box;
-  overflow: auto;
 }
 </style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,59 +1,133 @@
 <template>
   <div class="view-home">
-    <div class="view-home--content">
-      <div class="view-home--title">VUE-DATAGRID</div>
-      <div class="view-home--desc">满足各种复杂场景，高度可定制化表格组件</div>
-      <div>
-        <router-link class="view-home--link"
-                     to="/">Get Started</router-link>
-        <router-link class="view-home--link"
-                     to="/">Github</router-link>
+    <div class="view-home--header">
+      <div class="view-home--header-title">Vue-DataGrid</div>
+    </div>
+    <div class="view-home--body">
+      <div class="view-home--body-route">
+        <router-link class="view-home--body-link"
+                     to="/">基本功能</router-link>
+      </div>
+      <div class="view-home--body-content">
+        <datagrid :columns="columns"></datagrid>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import "../../packages/datagrid/assests/index.scss";
+import Datagrid from "../../packages/datagrid/components/table";
+
+export default {
+  components: { Datagrid },
+  data() {
+    return {
+      columns: [
+        { type: "checkbox", fixed: "left", width: 36 },
+        { key: "id", title: "id标识", fixed: "left", width: 100 },
+        {
+          title: "用户信息",
+          children: [
+            { key: "name", title: "姓名", width: 280 },
+            { key: "age", title: "年龄", width: 100 },
+            { key: "work", title: "工作", width: 100 }
+          ]
+        },
+        {
+          title: "地址",
+          children: [
+            { key: "street", title: "街道", width: 100 },
+            {
+              title: "详细地址",
+              children: [
+                { key: "building", title: "位置", width: 100 },
+                { key: "doorNo", title: "门牌号", width: 100 }
+              ]
+            }
+          ]
+        },
+        {
+          title: "指标",
+          children: [
+            { key: "target1", title: "指标1", width: 100 },
+            { key: "target2", title: "指标2", width: 100 },
+            { key: "target3", title: "指标3", width: 100 },
+            { key: "target4", title: "指标4", width: 100 }
+          ]
+        },
+        { key: "number0", title: "数值", fixed: "right", width: 100 },
+        { key: "number1", title: "数值1", fixed: "right", width: 100 },
+        { key: "number2", title: "数值2", fixed: "right", width: 100 },
+        { key: "number3", title: "数值3", fixed: "right", width: 100 }
+      ]
+    };
+  }
+};
 </script>
+
 <style lang="scss" scoped>
 .view-home {
   position: relative;
-  height: 100%;
-  width: 100%;
 }
 
-.view-home--content {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+.view-home--header {
+  transition: all 0.3s;
+  box-shadow: 0 2px 8px #f0f1f2;
+  position: relative;
+  z-index: 10;
+  height: 65px;
+  color: #444;
 }
-
-.view-home--title {
-  font-size: 48px;
-  font-weight: bold;
-  white-space: nowrap;
-}
-
-.view-home--desc {
+.view-home--header-title {
+  height: 65px;
+  line-height: 65px;
   font-size: 25px;
+  display: inline-block;
+  padding-left: 75px;
+  font-weight: 700;
 }
 
-.view-home--link {
-  display: inline-block;
-  height: 38px;
-  width: 120px;
-  border: 1px solid #fff;
-  border-radius: 19px;
-  color: #fff;
-  line-height: 38px;
-  letter-spacing: 0.5px;
-  text-align: center;
-  font-weight: normal;
-  margin-right: 10px;
-  background: #22ab28;
-  border-color: #209425;
+.view-home--body {
+  position: relative;
+  display: flex;
+  padding: 40px 0 0;
+  margin-left: 0;
+  margin-right: 0;
+  height: auto;
+  zoom: 1;
+  box-sizing: border-box;
+}
+
+.view-home--body-route {
+  width: 250px;
+  border-right: 1px solid #e8e8ee;
+}
+
+.view-home--body-link {
+  font-size: 16px;
+  color: #333;
+  line-height: 40px;
+  height: 40px;
+  margin: 0;
+  padding: 4px 25px;
+  text-decoration: none;
+  display: block;
+  position: relative;
+  transition: 0.15s ease-out;
+  font-weight: bold;
+  border-right: 3px solid transparent;
+  cursor: pointer;
+
+  &:hover {
+    color: #41a259;
+  }
+}
+
+.view-home--body-content {
+  flex: 1;
+  padding: 0 64px 64px 64px;
+  min-height: 500px;
 }
 </style>
 
