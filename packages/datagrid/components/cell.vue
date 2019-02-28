7ea8e055f5b0f1aa6f15a7a656c083de8462ac0a
kitorv <826526881@qq.com>
2019-02-28 16:44:08 +0800

    单元格组件

---
+++ packages/datagrid/components/cell.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  props: {
+    column: { type: Object },
+    data: { type: Object }
+  },
+  render(h) {
+    let { key } = this.column;
+    let { data } = this.data;
+    return <div>{data[key]}</div>;
+  }
+};
+</script>
