58241ad1459d392e80174d6b2e7f23f68a5c6cd4
kitorv <826526881@qq.com>
2019-02-28 15:53:23 +0800

    表头基本展示

---
+++ packages/datagrid/components/header.vue
@@ -0,0 +1,39 @@
+<template>
+  <table class="kv-datagrid--header-table"
+         cellspacing="0"
+         cellpadding="0"
+         border="0">
+    <colgroup>
+      <col v-for="(column, index) in leafColumns"
+           :key="index"
+           :style="getColStyle(column)">
+    </colgroup>
+    <tr v-for="(row, index) in columnRows"
+        :key="index">
+      <th v-for="(column, index) in row"
+          :key="index"
+          :colspan="column.colSpan"
+          :rowspan="column.rowSpan">
+        <div>{{column.title}}</div>
+      </th>
+    </tr>
+  </table>
+</template>
+
+<script>
+export default {
+  props: {
+    columnRows: { required: true, type: Array },
+    leafColumns: { required: true, type: Array }
+  },
+  methods: {
+    getColStyle({ width }) {
+      let widthStyle = `${width}px`;
+      return { width: widthStyle, minWidth: widthStyle };
+    }
+  },
+  created() {
+    console.log(this.columnRows);
+  }
+};
+</script>
