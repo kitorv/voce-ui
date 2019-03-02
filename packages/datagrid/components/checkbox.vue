558f8a1bcd312ee6171fca88227ac8439f0cadfb
kitorv <826526881@qq.com>
2019-03-02 17:59:51 +0800

    复选框组件

---
+++ packages/datagrid/components/checkbox.vue
@@ -0,0 +1,33 @@
+<template>
+  <label :class="['kv-datagrid-checkbox',{
+      'kv-datagrid-checkbox-checked':value,
+      'kv-datagrid-checkbox-indeterminate':!value&&indeterminate
+      }]">
+    <span class="kv-datagrid-checkbox--inner">
+      <input class="kv-datagrid-checkbox--inner-input"
+             type="checkbox"
+             v-model="model">
+    </span>
+  </label>
+
+</template>
+
+<script>
+export default {
+  props: {
+    value: { type: Boolean, default: false },
+    indeterminate: { type: Boolean, default: false }
+  },
+  computed: {
+    model: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit("input", value);
+      }
+    }
+  }
+};
+</script>
+
