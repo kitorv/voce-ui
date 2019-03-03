a065b57bf063b70b3fe37a04d4579f9417d2bd1b
kitorv <826526881@qq.com>
2019-03-03 11:21:13 +0800

    复选框扩展集成方式

--- packages/datagrid/columns/checkbox.js
+++ packages/datagrid/columns/checkbox.js
@@ -10,20 +10,37 @@ export default {
         on-click={() => {
           this.$set(dictionary, "checkAll", !dictionary.checkAll);
           this.$set(dictionary, "indeterminate", false);
+          this.dataSource.map(row => {
+            this.$set(row, "checked", dictionary.checkAll);
+          });
         }}
       />
     );
+  },
+  renderBody(h, { row, dictionary }) {
+    return (
+      <Checbox
+        value={row.checked}
+        on-click={() => {
+          this.$set(row, "checked", !row.checked);
+          let checkNumber = 0;
+          let dataSource = this.dataSource;
+          dataSource.forEach(row => {
+            if (!row.checked) return;
+            checkNumber += 1;
+          });
+          if (checkNumber == dataSource.length) {
+            this.$set(dictionary, "checkAll", true);
+            this.$set(dictionary, "indeterminate", false);
+          } else {
+            this.$set(dictionary, "checkAll", false);
+            this.$set(dictionary, "indeterminate", checkNumber > 0);
+          }
+        }}
+      />
+    );
+  },
+  rednerFooter() {
+    return <div />;
   }
-
-  // handleCheckClick() {
-  //   console.log(1);
-
-  // this.datagrid.checkedAll = !this.datagrid.checkedAll;
-  // this.datagrid.indeterminate = false;
-  // this.datagrid.dataSource.map(row => {
-  //   row.checked = this.datagrid.checkedAll;
-  // });
-  // }
-  // renderBody(h, { index, value, column, row }) {},
-  // renderFooter(h, { index, value, column, row }) {}
 };
