9858feca7d7c7053f51dab747d440bb064921ff0
kitorv <826526881@qq.com>
2019-03-04 15:18:09 +0800

    列自定义渲染统一接口化处理

--- packages/components/cell.vue
+++ packages/components/cell.vue
@@ -2,59 +2,32 @@
 import Checkbox from "./checkbox";
 
 export default {
-  data() {
-    return {
-      datagrid: this.$parent.$parent
-    };
-  },
   props: {
+    type: { type: String, default: "body" },
     column: { type: Object },
-    row: { type: Object }
+    row: { type: Object },
+    rowIndex: { type: Number }
   },
   render(h) {
-    // let { key, title, type, renderBody, renderFooter } = this.column;
-    // switch (type) {
-    //   case "checkbox":
-    //     return this.checkbox();
-    //   default:
     let { data } = this.row;
-    let { key } = this.column;
-    // let renderParams = {
-    //   row: this.row,
-    //   column: this.column,
-    //   value: data[key],
-    //   dictionary: this.datagrid.dictionary
-    // };
-    // if (renderBody && !this.footer) {
-    //   return renderBody.call(this.datagrid, h, renderParams);
-    // }
-    // if (renderFooter && this.footer) {
-    //   return renderBody.call(this.datagrid, h, renderParams);
-    // }
-    return <div>{data[key]}</div>;
-    // }
-  },
-  methods: {
-    checkbox() {
-      // return (
-      //   <Checkbox
-      //     value={this.row.checked}
-      //     nativeOn-click={() => {
-      //       let row = this.row;
-      //       row.checked = !row.checked;
-      //       let rowList = this.datagrid.dataSource;
-      //       let checkedRows = rowList.filter(row => row.checked);
-      //       if (rowList.length == checkedRows.length) {
-      //         this.datagrid.checkedAll = true;
-      //         this.datagrid.indeterminate = false;
-      //       } else {
-      //         this.datagrid.checkedAll = false;
-      //         this.datagrid.indeterminate = checkedRows.length > 0;
-      //       }
-      //     }}
-      //   />
-      // );
+    let { key, formatter, footerFormatter } = this.column;
+    if (formatter && this.type === "body") {
+      return formatter(h, {
+        row: this.row,
+        column: this.column,
+        value: data[key],
+        index: this.rowIndex
+      });
     }
+    if (footerFormatter && this.type === "footer") {
+      return footerFormatter(h, {
+        row: this.row,
+        column: this.column,
+        value: data[key],
+        index: this.rowIndex
+      });
+    }
+    return <div>{data[key]}</div>;
   }
 };
 </script>
