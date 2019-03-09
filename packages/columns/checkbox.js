5c27f1c789befc0fbd2efbfa1eb9e2a66fd83886
kitorv <826526881@qq.com>
2019-03-10 00:16:30 +0800

    复选框构建优化

--- packages/columns/checkbox.js
+++ packages/columns/checkbox.js
@@ -4,13 +4,20 @@ import TableCheckbox from "../components/checkbox";
 export default {
   width: 40,
   columnFormatter: function(h) {
-    <TableCheckbox
-      indeterminate={this.indeterminate}
-      value={this.checkedAll}
-      nativeOn-click={() => {
-        this.checkedAll = !this.checkedAll;
-      }}
-    />;
+    return (
+      <TableCheckbox
+        indeterminate={this.indeterminate}
+        value={this.checkedAll}
+        nativeOn-click={() => {
+          let checkedAll = !this.checkedAll;
+          this.checkedAll = checkedAll;
+          this.indeterminate = false;
+          this.dataSource.map(row => {
+            row.checked = checkedAll;
+          });
+        }}
+      />
+    );
   },
   formatter: function(h, { row }) {
     return (
