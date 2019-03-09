9918311a4396c0df85367eb2ebc4f15beed56575
kitorv <826526881@qq.com>
2019-03-09 22:53:46 +0800

    th td提取组件

--- packages/components/cell.vue
+++ packages/components/cell.vue
@@ -11,22 +11,35 @@ export default {
   render(h) {
     let { data } = this.row;
     let { key, headerFormatter, formatter, footerFormatter } = this.column;
-    let params = {
-      row: this.row,
-      column: this.column,
-      value: data[key],
-      index: this.rowIndex
-    };
-    if (headerFormatter && this.type === "header") {
-      return footerFormatter(h, params);
+    // let params = {
+    //   row: this.row,
+    //   column: this.column,
+    //   value: data[key],
+    //   index: this.rowIndex
+    // };
+    // if (headerFormatter && this.type === "header") {
+    //   return footerFormatter(h, params);
+    // }
+    // if (formatter && this.type === "body") {
+    //   return formatter(h, params);
+    // }
+    // if (footerFormatter && this.type === "footer") {
+    //   return footerFormatter(h, params);
+    // }
+    return <td class={this.tableCellClass}>{data[key]}</td>;
+  },
+  computed: {
+    tableCellClass() {
+      let { type, align } = this.column;
+      let classList = [];
+      if (type) {
+        classList.push(`kv-datagird--type-${type}`);
+      }
+      if (align) {
+        classList.push(`kv-datagird--align-${align}`);
+      }
+      return classList.join(" ");
     }
-    if (formatter && this.type === "body") {
-      return formatter(h, params);
-    }
-    if (footerFormatter && this.type === "footer") {
-      return footerFormatter(h, params);
-    }
-    return <div>{data[key]}</div>;
   }
 };
 </script>
---
+++ packages/components/row.vue
@@ -0,0 +1,43 @@
+<template>
+  <tr :class="[{'kv-datagird--row-hover':row.hover},{'kv-datagird--row-selected':row.selected},tableRowClass]"
+      @mouseenter="handleMouseEnter"
+      @mouseleave="handleMouseLeave"
+      @click="handleClick">
+    <td v-for="(column, cellIndex) in leafColumns"
+        :key="cellIndex"
+        is="table-cell"
+        :column="column"
+        :row="row"
+        :row-index="rowIndex">
+    </td>
+  </tr>
+</template>
+
+<script>
+import TableCell from "./cell";
+
+export default {
+  components: { TableCell },
+  props: {
+    row: { type: Object },
+    rowIndex: { type: Number },
+    leafColumns: { type: Array },
+    rowClass: { type: Function }
+  },
+  computed: {
+    tableRowClass() {
+      //   if (!this.rowClass) return "";
+      //   return this.rowClass.call(null, this.row, this.rowIndex);
+    }
+  },
+  methods: {
+    handleMouseEnter() {
+      this.row.hover = true;
+    },
+    handleMouseLeave() {
+      this.row.hover = false;
+    },
+    handleClick() {}
+  }
+};
+</script>
