a0b1e0734c495cdbc9f6b9fe7bf435aeafcf39ed
kitorv <826526881@qq.com>
2019-03-13 10:11:16 +0800

    展开行提取构建

---
+++ packages/components/expansion.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  inject: ["datagrid"],
+  props: {
+    row: { type: Object }
+  },
+  render(h) {
+    let slot = this.datagrid.$scopedSlots.expansion;
+    let datagridSlot = this.datagrid.$scopedSlots["expansion-datagrid"];
+    if (!slot && !datagridSlot) return;
+    let renderSlot = slot || datagridSlot;
+
+    return (
+      <tr
+        class={[
+          "kv-datagrid--expansion-row",
+          {
+            "kv-datagrid--expansion-datagrid": !slot && datagridSlot
+          }
+        ]}
+      >
+        <td colspan={this.datagrid.leafColumns.length}>
+          {renderSlot(this.row.data)}
+        </td>
+      </tr>
+    );
+  }
+};
+</script>
