2f7b4f532ff234e156f048b30a6ccaf6119a80dd
kitorv <826526881@qq.com>
2019-03-16 16:36:28 +0800

    代码初步展示

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import KvCodeSnippet from "./components/code-sinppet";
+import KvDatagrid from "../packages/components/table";
+
+import "../packages/style/index.scss";
 
 Vue.config.productionTip = false;
+Vue.component("kv-code-snippet", KvCodeSnippet);
+Vue.component("kv-datagrid", KvDatagrid);
 
 new Vue({
   router,
