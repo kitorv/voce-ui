0d56730a7a7681a149e3686b36c07e3ab2b678f1
kitorv <826526881@qq.com>
2019-03-21 09:44:29 +0800

    列配置优化处理

--- packages/store/column.js
+++ packages/store/column.js
@@ -1,26 +1,16 @@
-import Checkbox from "../columns/checkbox";
-import Index from "../columns/index";
-import Expansion from "../columns/expansion";
-import Tree from "../columns/tree";
+import checkbox from "../columns/checkbox";
+import index from "../columns/index";
+import expansion from "../columns/expansion";
+import tree from "../columns/tree";
+
+const columnMap = { checkbox, index, expansion, tree };
 
 export default function(column) {
   let { type } = column;
-  let extColumn = {};
-  switch (type) {
-    case "checkbox":
-      extColumn = Checkbox;
-      break;
-    case "index":
-      extColumn = Index;
-      break;
-    case "expansion":
-      extColumn = Expansion;
-      break;
-    case "tree":
-      extColumn = Tree;
-      break;
+  let extColumn = columnMap[type];
+  if (extColumn) {
+    Object.assign(column, extColumn);
   }
-  Object.assign(column, extColumn);
   column.isLeaf = true;
   column.colStyle = {
     width: `${column.width}px`,
