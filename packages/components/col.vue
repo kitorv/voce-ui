8df954993bde7c2c023ff557f8300ad9a96bc0e0
kitorv <826526881@qq.com>
2019-03-23 00:08:51 +0800

    默认加宽序号显示长度

---
+++ packages/components/col.vue
@@ -0,0 +1,22 @@
+<template>
+  <col :style="style">
+</template>
+
+<script>
+export default {
+  inject: ['datagrid'],
+  props: {
+    column: { type: Object }
+  },
+  computed: {
+    style() {
+      let { width, type } = this.column
+      return {
+        width: `${width}px`,
+        minWidth: `${width}px`
+      }
+    }
+  }
+}
+</script>
+
