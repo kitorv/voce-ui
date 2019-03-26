554a1b9377f5dbe0d5eac354bf0dd5fb494e63cc
kitorv <826526881@qq.com>
2019-03-26 22:57:25 +0800

    无数据表头的复选框禁用处理 去掉选中状态

--- packages/components/checkbox.vue
+++ packages/components/checkbox.vue
@@ -1,12 +1,14 @@
 <template>
   <label :class="['kv-datagrid-checkbox',{
       'kv-datagrid-checkbox-checked':value,
-      'kv-datagrid-checkbox-indeterminate':!value&&indeterminate
+      'kv-datagrid-checkbox-indeterminate':!value&&indeterminate,
+      'kv-datagrid-checkbox-disabled':disabled
       }]">
     <span class="kv-datagrid-checkbox--inner">
       <input class="kv-datagrid-checkbox--inner-input"
              type="checkbox"
-             v-model="model">
+             v-model="model"
+             :disabled="disabled">
     </span>
   </label>
 
@@ -16,7 +18,8 @@
 export default {
   props: {
     value: { type: Boolean, default: false },
-    indeterminate: { type: Boolean, default: false }
+    indeterminate: { type: Boolean, default: false },
+    disabled: { type: Boolean, default: false }
   },
   computed: {
     model: {
