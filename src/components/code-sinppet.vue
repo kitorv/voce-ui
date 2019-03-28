b7704f4448f6c4775945e03b3da55171209302e6
kitorv <826526881@qq.com>
2019-03-28 23:54:10 +0800

    部分属性配置文档

--- src/components/code-sinppet.vue
+++ src/components/code-sinppet.vue
@@ -104,27 +104,7 @@ export default {
     padding-left: 4px;
     padding-top: 15px;
   }
-  > table {
-    border-collapse: collapse;
-    border-spacing: 0;
-    empty-cells: show;
-    border: 1px solid #ebedf0;
-    width: 100%;
-    margin: 8px 0 16px;
-  }
-  > table th,
-  > table td {
-    color: #314659;
-    border: 1px solid #ebedf0;
-    text-align: left;
-    padding: 10px 15px;
-  }
-  > table th {
-    white-space: nowrap;
-    color: #5c6b77;
-    font-weight: 500;
-    background: rgba(0, 0, 0, 0.02);
-  }
+
   strong,
   b {
     font-weight: 500;
