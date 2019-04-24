9148327882756e290f6d8b137c937a47106991b6
kitorv <826526881@qq.com>
2019-04-24 17:52:40 +0800

    复选框基本功能完成

--- packages/components/checkbox/checkbox.vue
+++ packages/components/checkbox/checkbox.vue
@@ -13,19 +13,44 @@ export default {
   name: "KvCheckbox",
   componentName: "KvCheckbox",
   data() {
-    return {}
+    return {
+      checkboxGroup: null
+    }
   },
   computed: {
+    kvCheckboxGroup() {
+      if (this.checkboxGroup) return this.checkboxGroup
+      let parent = this.$parent;
+      while (parent) {
+        if (parent.$options.componentName !== 'KvCheckboxGroup') {
+          parent = parent.$parent;
+        } else {
+          this.checkboxGroup = parent;
+          return this.checkboxGroup
+        }
+      }
+    },
     model: {
       get() {
-        if ({}.toString.call(this.value) === '[object Boolean]') {
-          return this.value;
+        if (this.kvCheckboxGroup) {
+          return this.kvCheckboxGroup.value.includes(this.value)
         }
+        return value
       },
       set(value) {
-        this.$emit('input', value);
+        if (!this.kvCheckboxGroup) {
+          this.$emit('input', value);
+          return
+        }
+        const index = this.kvCheckboxGroup.findIndex(v => v == this.value)
+        if (index > -1) {
+          this.kvCheckboxGroup.value.splice(index, 1)
+        } else {
+          this.kvCheckboxGroup.value.push(this.value)
+        }
+        this.kvCheckboxGroup.$emit('input', this.kvCheckboxGroup.value)
       }
-    }
+    },
   },
   props: {
     value: [Boolean, Number, String]
