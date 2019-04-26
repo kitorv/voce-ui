3ebe7332d8e9d181957e5c2be7124564801e884d
kitorv <826526881@qq.com>
2019-04-26 15:40:41 +0800

    文本框图标

--- packages/components/input/input.vue
+++ packages/components/input/input.vue
@@ -1,15 +1,30 @@
 <template>
-  <div :class="['kv-input']">
+  <div :class="['kv-input',
+        {'kv-input--disabled':disabled},
+        {'kv-input--suffix':$slots.suffix},
+        {'kv-input--prefix':$slots.prefix}]">
+    <span class="kv-input--prefix-inner"
+          v-if="$slots.prefix">
+      <slot name="prefix"></slot>
+    </span>
     <input type="text"
            class="kv-input--input"
-           v-bind="$attrs">
+           v-bind="$attrs"
+           :disabled="disabled">
+    <span class="kv-input--suffix-inner"
+          v-if="$slots.suffix">
+      <slot name="suffix"></slot>
+    </span>
   </div>
 </template>
 
 <script>
 export default {
   name: 'KvInput',
-  componentName: 'KvInput'
+  componentName: 'KvInput',
+  props: {
+    disabled: Boolean
+  }
 }
 </script>
 
