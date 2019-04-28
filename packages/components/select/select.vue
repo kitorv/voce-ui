a288a336bc95902a8fa7084234072d7c1985a90a
kitorv <826526881@qq.com>
2019-04-28 15:44:17 +0800

    clickoutside指令

--- packages/components/select/select.vue
+++ packages/components/select/select.vue
@@ -1,13 +1,21 @@
 <template>
-  <div>
+  <div v-clickoutside="handleClickOutside">
     #select
   </div>
 </template>
 
 <script>
+import clickoutside from "../../directives/clickoutside.js"
+
 export default {
   name: 'KvSelect',
-  componentName: 'KvSelect'
+  componentName: 'KvSelect',
+  directives: { clickoutside },
+  methods: {
+    handleClickOutside() {
+      console.log(1);
+    }
+  },
 }
 </script>
 
