dc6a4d27e920a96c15e2ef154dc4efc02a9df994
kitorv <826526881@qq.com>
2019-04-28 10:49:44 +0800

    文本域基本功能

--- packages/components/textarea/index.md
+++ packages/components/textarea/index.md
@@ -2,11 +2,11 @@
 
 ## 基础用法
 
-:::snippet
+:::snippet `kv-textarea` 初始化，默认宽度百分吧。
 
 ```html
 <template>
-  <div class="kv-demo--input">
+  <div class="kv-demo--textarea">
     <kv-textarea placeholder="请输入内容" v-model="value"></kv-textarea>
   </div>
 </template>
@@ -19,6 +19,75 @@
     }
   };
 </script>
+<style>
+  .kv-demo--textarea .kv-textarea {
+    width: 400px;
+  }
+</style>
+```
+
+:::
+
+## 内容高度自适应
+
+:::snippet 设置 `autosize` 属性为 `true` 根据内容适应高度。
+
+```html
+<template>
+  <div class="kv-demo--textarea">
+    <kv-textarea
+      placeholder="内容无限加高"
+      v-model="value"
+      autosize
+    ></kv-textarea>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        value: ""
+      };
+    }
+  };
+</script>
+<style>
+  .kv-demo--textarea .kv-textarea {
+    width: 400px;
+  }
+</style>
+```
+
+:::
+
+## 内容高度限制
+
+:::snippet 设置 `autosize` 属性为 `{minRows,maxRows}` 根据内容适应高度。
+
+```html
+<template>
+  <div class="kv-demo--textarea">
+    <kv-textarea
+      placeholder="限制从2行到5行超出滚动条"
+      v-model="value"
+      :autosize="{minRows: 2,maxRows: 5}"
+    ></kv-textarea>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        value: ""
+      };
+    }
+  };
+</script>
+<style>
+  .kv-demo--textarea .kv-textarea {
+    width: 400px;
+  }
+</style>
 ```
 
 :::
--- packages/components/textarea/textarea.vue
+++ packages/components/textarea/textarea.vue
@@ -1,13 +1,67 @@
 <template>
-  <div>
-    textarea
+  <div :class="['kv-textarea',{
+        'kv-textarea-disabled':disabled
+    }]">
+    <textarea class="kv-textarea--content"
+              v-bind="$attrs"
+              :disabled="disabled"
+              :style="textareaStyle"
+              :rows="rows"
+              v-model="model"
+              ref="textarea">
+        </textarea>
   </div>
 </template>
 
 <script>
+import calcTextareaHeight from './calcTextareaHeight.js'
+
 export default {
   name: "KvTextarea",
-  componentName: "KvTextarea"
+  componentName: "KvTextarea",
+  data() {
+    return {
+      textareaStyle: {}
+    }
+  },
+  props: {
+    value: [String, Number],
+    disabled: Boolean,
+    autosize: {
+      type: [Boolean, Object],
+      default: false
+    },
+    rows: {
+      type: Number,
+      default: 2
+    }
+  },
+  computed: {
+    model: {
+      get() {
+        return this.value
+      },
+      set(value) {
+        this.$emit("input", value)
+      }
+    }
+  },
+  methods: {
+    resizeTextarea() {
+      if (!this.$refs.textarea || !this.autosize) return
+      const minRows = this.autosize.minRows || this.rows;
+      const maxRows = this.autosize.maxRows;
+      this.textareaStyle = calcTextareaHeight(this.$refs.textarea, minRows, maxRows);
+    }
+  },
+  watch: {
+    model() {
+      this.resizeTextarea()
+    }
+  },
+  mounted() {
+    this.resizeTextarea()
+  }
 };
 </script>
 
