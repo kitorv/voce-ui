56ba732d2cc27735826396760621d959cf0ce7e6
kitorv <826526881@qq.com>
2019-04-29 09:41:08 +0800

    文件目录结构调整

--- packages/index.js
+++ packages/index.js
@@ -1,10 +1,8 @@
 import KvRow from "./components/row";
 import KvCol from "./components/col";
 import KvButton from "./components/button";
-import KvChcekboxGroup from "./components/checkbox-group";
 import KvChcekbox from "./components/checkbox";
-import KvRadio from "./components/radio-group";
-import KvRadioGroup from "./components/radio";
+import KvRadio from "./components/radio";
 import KvInput from "./components/input";
 import KvTeatarea from "./components/textarea";
 import KvSelect from "./components/select";
@@ -14,9 +12,7 @@ const components = [
   KvRow,
   KvCol,
   KvButton,
-  KvChcekboxGroup,
   KvChcekbox,
-  KvRadioGroup,
   KvRadio,
   KvInput,
   KvTeatarea,
@@ -26,7 +22,7 @@ const components = [
 
 const install = function(Vue) {
   components.forEach(component => {
-    Vue.component(component.name, component);
+    Vue.use(component);
   });
 };
 
