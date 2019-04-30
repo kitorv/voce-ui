83cfbf9ad7d824e107073aad736c8b38a09cfe30
kitorv <826526881@qq.com>
2019-04-30 17:38:56 +0800

    下拉框分组

---
+++ packages/components/select/group.vue
@@ -0,0 +1,19 @@
+<template>
+  <li class="kv-select-option-group">
+    <ul class="kv-select-option-group--content">
+      <li class="kv-select-option-group--title">{{title}}</li>
+      <slot></slot>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'KvOptionGroup',
+  componentName: 'KvOptionGroup',
+  props: {
+    title: String
+  }
+}
+</script>
+
--- packages/components/select/index.js
+++ packages/components/select/index.js
@@ -1,8 +1,10 @@
 import KvSelect from "./select.vue";
+import KvOptionGroup from "./group.vue";
 import KvOption from "./option.vue";
 
 KvSelect.install = function(Vue) {
   Vue.component(KvSelect.name, KvSelect);
+  Vue.component(KvOptionGroup.name, KvOptionGroup);
   Vue.component(KvOption.name, KvOption);
 };
 
