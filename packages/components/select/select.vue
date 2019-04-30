16d17ad9993967ad3bda54a174ebc22bc0868033
kitorv <826526881@qq.com>
2019-04-30 10:32:09 +0800

    基本的多选操作

--- packages/components/select/select.vue
+++ packages/components/select/select.vue
@@ -40,14 +40,26 @@ export default {
   },
   methods: {
     handleOptionClick(kvOption) {
-      if (this.multiple) {
-        // 多选
-      } else {
-        // 单选
+      let value = this.value
+      const optionValue = kvOption.value
+      if (!Array.isArray(value)) {
         this.selectText = kvOption.text
         this.visable = false
-        this.$emit('input', kvOption.value)
+        this.$emit('input', optionValue)
+        return
       }
+      const optionIndex = this.value.findIndex(m => m === optionValue)
+      if (optionIndex > -1) {
+        value.splice(optionIndex, 1);
+      } else {
+        if (this.multiple) {
+          value.push(optionValue)
+        } else {
+          value = [optionValue]
+        }
+      }
+      this.selectText = kvOption.text = value.join(",")
+      this.$emit('input', value)
     }
   }
 }
