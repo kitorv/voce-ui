5b08867f982c94729fada78feafc12a25dc54ea0
kitorv <826526881@qq.com>
2019-05-01 11:31:22 +0800

    下拉框基础数据过滤

--- packages/components/select/select.vue
+++ packages/components/select/select.vue
@@ -1,21 +1,21 @@
 <template>
   <kv-dropdown :class="['kv-select',{'kv-select--disabled':disabled}]"
-               :visable.sync="visable"
+               :visible.sync="visible"
                :disabled="disabled">
     <div slot="selection"
          class="kv-select--selection">
       <input class="kv-select--input"
              v-if="mode!=='tags'"
-             :value="selectText"
+             v-model="queryText"
              :placeholder="placeholder"
              :disabled="disabled"
              :readonly="!filter"
              @input="handleFilterOption" />
       <div v-else
            class="kv-select--tags">
-        <div v-if="selectText&&selectText.length<1"
+        <div v-if="selectOptions&&selectOptions.length<1"
              class="kv-select--placeholder">{{placeholder}}</div>
-        <span v-for="(option, index) in selectText"
+        <span v-for="(option, index) in selectOptions"
               :key="index"
               class="kv-select--tags-item">{{option.text}}</span>
       </div>
@@ -28,24 +28,28 @@
     <ul slot="panel"
         class="kv-select--dropdown">
       <slot></slot>
+      <div v-if="showEmpty"
+           class="kv-select--empty">暂无数据</div>
     </ul>
   </kv-dropdown>
 </template>
 
 <script>
-import KvDropdown from "../dropdown/dropdown"
+import KvDropdown from "../dropdown/dropdown";
 
 export default {
-  name: 'KvSelect',
-  componentName: 'KvSelect',
+  name: "KvSelect",
+  componentName: "KvSelect",
   provide() {
-    return { kvSelect: this }
+    return { kvSelect: this };
   },
   data() {
     return {
-      visable: false,
-      options: []
-    }
+      visible: false,
+      options: [],
+      queryText: "",
+      showEmpty: false
+    };
   },
   props: {
     mode: String,
@@ -55,7 +59,7 @@ export default {
     },
     valueKey: {
       type: String,
-      default: 'value'
+      default: "value"
     },
     placeholder: {
       type: String,
@@ -68,45 +72,68 @@ export default {
     }
   },
   computed: {
+    selectOptions() {
+      const selectValue = Array.isArray(this.value) ? this.value : [this.value];
+      return this.options.filter(m => selectValue.includes(m.value));
+    },
     selectText() {
-      const selectValue = Array.isArray(this.value) ? this.value : [this.value]
-      const selectText = []
-      if (this.mode == "tags") {
-        return this.options.filter(m => selectValue.includes(m.value))
-      }
-      this.options.forEach(option => {
-        if (!selectValue.includes(option.value)) return
-        selectText.push(option.text)
-      });
-      return selectText.join(",")
+      return Array.from(this.selectOptions, m => m.text).join(",");
     }
   },
   methods: {
     handleOptionClick(kvOption) {
-      let value = this.value
-      const optionValue = kvOption.value
+      let value = this.value;
+      const optionValue = kvOption.value;
       if (!Array.isArray(value)) {
-        this.visable = false
-        this.$emit('input', optionValue)
-        return
+        this.visible = false;
+        this.$emit("input", optionValue);
+        return;
       }
-      const optionIndex = this.value.findIndex(m => m === optionValue)
+      const optionIndex = this.value.findIndex(m => m === optionValue);
       if (optionIndex > -1) {
         value.splice(optionIndex, 1);
       } else {
         if (this.mode) {
-          value.push(optionValue)
+          value.push(optionValue);
         } else {
-          value = [optionValue]
+          value = [optionValue];
         }
       }
-      this.$emit('input', value)
+      this.$emit("input", value);
     },
-    handleFilterOption(event) {
-      console.log(event);
-
+    handleFilterOption() {
+      const value = this.queryText;
+      const parsedValue = value.replace(
+        /(\^|\(|\)|\[|\]|\$|\*|\+|\.|\?|\\|\{|\}|\|)/g,
+        "\\$1"
+      );
+      let showOptionNumber = 0;
+      this.options.forEach(option => {
+        option.visible = new RegExp(parsedValue, "i").test(option.text);
+        if (!option.visible) return;
+        showOptionNumber++;
+      });
+      this.showEmpty = showOptionNumber < 1;
+    },
+    setQueryText() {
+      this.queryText = this.selectText;
     }
+  },
+  watch: {
+    value() {
+      this.setQueryText();
+    },
+    visible(value) {
+      if (value) {
+        this.handleFilterOption();
+      } else {
+        this.setQueryText();
+      }
+    }
+  },
+  mounted() {
+    this.setQueryText();
   }
-}
+};
 </script>
 
