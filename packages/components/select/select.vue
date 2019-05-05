4e4df918fa9f0cdbe26bb156411a6abbaa248a98
kitorv <826526881@qq.com>
2019-05-05 15:01:58 +0800

    下拉框的初始化赋值和选中

--- packages/components/select/select.vue
+++ packages/components/select/select.vue
@@ -6,12 +6,16 @@
          class="kv-select--selection">
       <div>{{value}}</div>
       <input class="kv-select--input"
+             v-model="inputText"
+             :placeholder="placeholder"
+             :readonly="true" />
+      <!-- <input class="kv-select--input"
              v-if="mode!=='tags'"
              v-model="queryText"
              :placeholder="placeholder"
              :readonly="!filter"
-             @input="handleInputFilter" />
-      <div v-else
+             @input="handleInputFilter" /> -->
+      <!-- <div v-else
            class="kv-select--tags">
         <div v-if="selectOptions&&selectOptions.length<1"
              class="kv-select--placeholder">{{placeholder}}</div>
@@ -24,13 +28,13 @@
         <slot name="icon">
           <i class="kv-icon-down"></i>
         </slot>
-      </span>
+      </span> -->
     </div>
     <ul slot="panel"
         class="kv-select--dropdown">
       <slot></slot>
-      <div v-if="showEmpty"
-           class="kv-select--empty">暂无数据</div>
+      <!-- <div v-if="showEmpty"
+           class="kv-select--empty">暂无数据</div> -->
     </ul>
   </kv-dropdown>
 </template>
@@ -48,86 +52,110 @@ export default {
   data() {
     return {
       visible: false,
-      options: [],
-      queryText: "",
-      showEmpty: false
+      dataList: {},
+      inputText: ""
+      // optionList: []
+      // options: [],
+      // queryText: "",
+      // showEmpty: false
     };
   },
   props: {
-    mode: String,
+    disabled: Boolean,
     value: { required: true },
+    valueKey: String,
+    labelKey: String,
     placeholder: {
       type: String,
       default: "请选择"
     },
-    disabled: Boolean,
-    filter: {
-      type: [Boolean, Function],
-      default: false
-    }
+    // mode: String,
+
+    // filter: {
+    //   type: [Boolean, Function],
+    //   default: false
+    // }
   },
   computed: {
-    selectOptions() {
-      const selectValue = Array.isArray(this.value) ? this.value : [this.value];
-      return this.options.filter(m => selectValue.includes(m.value));
+    selectValue: {
+      get() {
+        if (Array.isArray(this.value)) {
+          return this.valueKey ? Array.from(this.value, value => value[this.valueKey]) : this.value
+        }
+        if (this.valueKey) {
+          return [this.value[this.valueKey]]
+        }
+        return [this.value]
+      },
+      set() {
+
+      }
     },
     selectText() {
-      return Array.from(this.selectOptions, m => m.text).join(",");
+      return Array.from(this.selectValue, value => {
+        const data = this.dataList[value]
+        if (data === undefined) return
+        return data.text
+      }).join(",");
     }
+    // selectOptions() {
+    //   const selectValue = Array.isArray(this.value) ? this.value : [this.value];
+    //   return this.options.filter(m => selectValue.includes(m.value));
+    // },
   },
   methods: {
     handleOptionClick(kvOption) {
-      if (!Array.isArray(this.value)) {
-        kvOption.select();
-        this.visible = false;
-        return;
-      }
-      if (kvOption.selected) {
-        kvOption.unselect();
-        return;
-      }
-      kvOption.select();
+      // if (!Array.isArray(this.value)) {
+      //   kvOption.select();
+      //   this.visible = false;
+      //   return;
+      // }
+      // if (kvOption.selected) {
+      //   kvOption.unselect();
+      //   return;
+      // }
+      // kvOption.select();
     },
-    handleTagClick(kvOption) {
-      kvOption.unselect();
-    },
-    handleInputFilter() {
-      this.visible = true;
-      this.filterOption();
-    },
-    filterOption() {
-      const value = this.queryText;
-      const parsedValue = value.replace(
-        /(\^|\(|\)|\[|\]|\$|\*|\+|\.|\?|\\|\{|\}|\|)/g,
-        "\\$1"
-      );
-      let showOptionNumber = 0;
-      this.options.forEach(option => {
-        option.visible = new RegExp(parsedValue, "i").test(option.text);
-        if (!option.visible) return;
-        showOptionNumber++;
-      });
-      this.showEmpty = showOptionNumber < 1;
-    },
-    setInputText() {
-      this.queryText = this.selectText;
-    }
+    // handleTagClick(kvOption) {
+    //   kvOption.unselect();
+    // },
+    // handleInputFilter() {
+    //   this.visible = true;
+    //   this.filterOption();
+    // },
+    // filterOption() {
+    //   const value = this.queryText;
+    //   const parsedValue = value.replace(
+    //     /(\^|\(|\)|\[|\]|\$|\*|\+|\.|\?|\\|\{|\}|\|)/g,
+    //     "\\$1"
+    //   );
+    //   let showOptionNumber = 0;
+    //   this.options.forEach(option => {
+    //     option.visible = new RegExp(parsedValue, "i").test(option.text);
+    //     if (!option.visible) return;
+    //     showOptionNumber++;
+    //   });
+    //   this.showEmpty = showOptionNumber < 1;
+    // },
+    // setInputText() {
+    //   this.queryText = this.selectText;
+    // }
   },
   watch: {
-    value() {
-      this.setInputText();
-    },
-    visible(value) {
-      if (value && this.filter) {
-        this.filterOption();
-      }
-      if (!value) {
-        this.setInputText();
-      }
-    }
+    // value() {
+    //   this.setInputText();
+    // },
+    // visible(value) {
+    //   if (value && this.filter) {
+    //     this.filterOption();
+    //   }
+    //   if (!value) {
+    //     this.setInputText();
+    //   }
+    // }
   },
   mounted() {
-    this.setInputText();
+    this.inputText = this.selectText
   }
 };
 </script>
