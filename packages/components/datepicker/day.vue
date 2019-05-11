48a9df97f7650f15ba8f33251ebd3976c21527ed
kitorv <826526881@qq.com>
2019-05-11 11:35:06 +0800

    日期初始化代码优化

--- packages/components/datepicker/day.vue
+++ packages/components/datepicker/day.vue
@@ -19,17 +19,7 @@
 </template>
 
 <script>
-import {
-  startOfMonth,
-  getDay,
-  subDays,
-  addDays,
-  isToday,
-  isBefore,
-  lastDayOfMonth,
-  isSameDay,
-  getDate
-} from "date-fns";
+import dateFns from "date-fns";
 
 export default {
   name: "KvDateDay",
@@ -47,59 +37,51 @@ export default {
     date: {
       type: Date,
       required: true
-    }
+    },
+    disabled: Function
   },
   computed: {
     dateRows() {
-      const firstDate = startOfMonth(this.date);
-      const diffDay = getDay(firstDate);
-      if (diffDay === 0) {
-        diffDay = 7;
-      }
-      const startDate = subDays(firstDate, diffDay);
-      const rows = this.dateRowList;
+      const firstDate = dateFns.startOfMonth(this.date);
+      let diffDay = dateFns.getDay(firstDate) || 7;
+      const startDate = dateFns.subDays(firstDate, diffDay);
       for (let i = 0; i < 6; i++) {
-        const row = rows[i];
+        const row = this.dateRowList[i];
         for (let j = 0; j < 7; j++) {
-          // 获取单元格的值
-          let cell = row[j];
-          // 单元格不可用设置初始化配置
-          if (!cell) {
-            cell = { row: i, column: j, type: "normal", disabled: false };
+          let cell = row[j] || {
+            row: i,
+            column: j,
+            type: "normal",
+            disabled: false
+          };
+          const currentDate = dateFns.addDays(startDate, i * 7 + j);
+          let type = "normal";
+          if (dateFns.isToday(currentDate)) {
+            type = "today";
           }
-          cell.type = "normal";
-          const currentDate = addDays(startDate, i * 7 + j);
-          if (isToday(currentDate)) {
-            cell.type = "today";
+          if (dateFns.isBefore(currentDate, dateFns.startOfMonth(this.date))) {
+            type = "prev-month";
           }
-          if (isBefore(currentDate, startOfMonth(this.date))) {
-            cell.type = "prev-month";
+          if (dateFns.isAfter(currentDate, dateFns.lastDayOfMonth(this.date))) {
+            type = "next-month";
           }
-          if (isBefore(startOfMonth(this.date), lastDayOfMonth(this.date))) {
-            cell.type = "prev-month";
-          }
-
-          // 判断是否当前选中时间
           if (
-            cell.type != "prev-month" &&
-            cell.type != "next-month" &&
-            isSameDay(currentDate, this.date)
+            type != "prev-month" &&
+            type != "next-month" &&
+            dateFns.isSameDay(currentDate, this.date)
           ) {
-            cell.type = "select";
+            type = "select";
           }
-          //                     // 判断该日期是否禁用
-          //                     if (this.disabledDate) {
-          //                         cell.disabled = this.disabledDate(moment(date));
-          //                     }
-          // 设置时间
-          cell.text = getDate(currentDate);
-          // 完整时间存储
+          cell.type = type;
+          if (this.disabled) {
+            cell.disabled = this.disabledDate(moment(date));
+          }
+          cell.text = dateFns.getDate(currentDate);
           cell.date = currentDate;
-          // 添加到状态管理
           this.$set(row, j, cell);
         }
       }
-      return rows;
+      return this.dateRowList;
     }
   }
 };
