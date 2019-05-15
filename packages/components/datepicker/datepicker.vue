d952d3f165d3e36a77cac60605aadf49517d739b
kitorv <826526881@qq.com>
2019-05-15 16:29:19 +0800

    时间框基本功能选择

--- packages/components/datepicker/datepicker.vue
+++ packages/components/datepicker/datepicker.vue
@@ -1,6 +1,22 @@
 <template>
-  <div>
-    {{dateText}}
+  <kv-dropdown class="kv-date-picker"
+               :visible.sync="visible">
+    <div slot="selection"
+         class="kv-date-picker--selection">
+      <input class="kv-date-picker--input"
+             v-model="inputText" />
+      <span class="kv-date-picker--icon">
+        <i class="kv-icon-calendar"></i>
+      </span>
+    </div>
+    <div slot="panel"
+         class="kv-date-picker--panel">
+      <kv-date-calendar :date="dateValue"
+                        :select-value="dateValue"
+                        @date-click="handleDateClick"></kv-date-calendar>
+    </div>
+  </kv-dropdown>
+  <!-- {{dateText}}
     <br>
     <kv-date-time :date="date"></kv-date-time>
     <br>
@@ -13,29 +29,120 @@
     <br>
     <kv-date-calendar :date="date"
                       type="year"
-                      v-model="date"></kv-date-calendar>
-  </div>
+                      v-model="date"></kv-date-calendar> -->
 </template>
 
 <script>
+import KvDropdown from "../dropdown/dropdown"
 import KvDateCalendar from "./calendar";
-import KvDateTime from "./time"
 import dateFns from "date-fns"
+// import KvDateTime from "./time"
 
 export default {
   name: "KvDatePicker",
   componentName: "KvDatePicker",
-  components: { KvDateCalendar, KvDateTime },
+  components: { KvDateCalendar },
   data() {
     return {
-      date: new Date()
-    };
+      visible: false,
+      inputText: null
+    }
+  },
+  props: {
+    // TODO 区间
+    value: [String, Array]
   },
   computed: {
+    dateValue: {
+      get() {
+        return dateFns.parse(this.value)
+      },
+      set(value) {
+        // TODO 多种情况判断
+        this.$emit('input', dateFns.format(value, "YYYY-MM-DD"))
+      }
+    },
     dateText() {
-      return dateFns.format(this.date, "YYYY-MM-DD HH:mm:ss")
+      return dateFns.format(this.dateValue, "YYYY-MM-DD")
+    }
+  },
+  methods: {
+    handleDateClick(value) {
+      this.dateValue = value
+    }
+  },
+  watch: {
+    dateValue: {
+      immediate: true,
+      handler() {
+        this.inputText = this.dateText
+      }
     }
   }
 };
 </script>
+
+<style lang="scss">
+@import "../../style/variable.scss";
+@import "../dropdown/index.scss";
+
+.kv-date-picker {
+  position: relative;
+  display: inline-block;
+  font-size: 14px;
+  zoom: 1;
+}
+
+.kv-date-picker--selection {
+  position: relative;
+  box-sizing: border-box;
+}
+
+.kv-date-picker--input {
+  background-color: $--select-background-color;
+  border: $--select-border;
+  border-radius: $--select-border-radius;
+  -webkit-appearance: none;
+  background-image: none;
+  box-sizing: border-box;
+  display: block;
+  font-size: 14px;
+  line-height: 1.5;
+  outline: none;
+
+  transition: border-color 0.2s;
+  width: 100%;
+
+  &.kv-date-picker--input {
+    padding: 4px 30px 4px 11px;
+    height: 32px;
+
+    &::placeholder {
+      font-size: 14px;
+      color: $--select-border-color;
+    }
+  }
+}
+
+.kv-date-picker--icon {
+  position: absolute;
+  top: 50%;
+  right: 10px;
+  z-index: 2;
+  line-height: 0;
+  transform: translateY(-50%);
+  color: $--select-icon-color;
+  font-size: 18px;
+}
+
+.kv-date-picker--panel {
+  background-color: $--select-dropdown-background-color;
+  padding: 4px;
+  margin: 4px 0;
+  box-shadow: 0 5px 20px 0 rgba(0, 0, 0, 0.08);
+  border-radius: 3px;
+  z-index: 1050;
+}
+</style>
+
 
