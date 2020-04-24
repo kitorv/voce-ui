0ba33414ae2f0d43534e66fa651af38c660a1f0d
kitorv <826526881@qq.com>
2020-04-24 22:38:10 +0800

    路由配置

--- site/App.vue
+++ site/App.vue
@@ -1,21 +1,28 @@
 <template>
-  <div>
-    <h1>vuce-ui</h1>
-    <start-doc-view></start-doc-view>
-  </div>
+  <router-view></router-view>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import StartDocView from "./docs/start.md";
-
-export default defineComponent({
-  components: { StartDocView },
-});
+import { defineComponent } from 'vue';
+export default defineComponent({});
 </script>
 
 <style lang="scss">
-h1 {
-  color: $-color-primary;
+body,
+html {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB,
+    Microsoft YaHei, SimSun, sans-serif;
+  font-weight: 400;
+  -webkit-font-smoothing: antialiased;
+  -webkit-tap-highlight-color: transparent;
+}
+
+#app {
+  // TODO
+  height: 100%;
+  color: #333333;
 }
 </style>
---
+++ site/views/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="v-index">
+    <div class="v-index--header">
+      <div class="v-index--header-title">Veco</div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.v-index--header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 3rem;
+  padding: 0.4rem 1rem;
+  line-height: 1.5rem;
+  position: relative;
+  background: #fff;
+  z-index: 1200;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  box-shadow: 0 0 15px 0 rgba(0, 0, 0, 0.04);
+}
+
+.v-index--header-title {
+  font-size: 22px;
+  font-weight: 700;
+}
+</style>
