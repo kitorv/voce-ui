3acbe089fd03add6d38cb07683f3c598cee48ca1
kitorv <826526881@qq.com>
2020-07-13 21:51:15 +0800

    Divider 文字描述

--- src/components/divider/README.md
+++ src/components/divider/README.md
@@ -19,3 +19,23 @@
 ```
 
 :::
+
+## 文字描述
+
+:::snippet 分割线中带有文字，可以用`orientation`指定文字位置。
+
+```vue
+<template>
+  <div class="vsc-divider">
+    <p>A Vue.js 3.0 UI Toolkit for Web</p>
+    <v-divider orientation="left">Left Text</v-divider>
+    <p>A Vue.js 3.0 UI Toolkit for Web</p>
+    <v-divider>Text</v-divider>
+    <p>A Vue.js 3.0 UI Toolkit for Web</p>
+    <v-divider orientation="right">Right Text</v-divider>
+    <p>A Vue.js 3.0 UI Toolkit for Web</p>
+  </div>
+</template>
+```
+
+:::
--- src/components/divider/src/index.vue
+++ src/components/divider/src/index.vue
@@ -1,11 +1,17 @@
 <template>
-  <div :class="classList"></div>
+  <div :class="classList">
+    <span v-if="showText" class="v-divider--text">
+      <slot />
+    </span>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from "vue";
 
 export type DividerType = "horizontal" | "vertical";
+
+export type OrientationType = "left" | "center" | "right";
 
 export default defineComponent({
   name: "VDivider",
@@ -14,38 +20,53 @@ export default defineComponent({
       type: String as PropType<DividerType>,
       default: "horizontal",
     },
+    orientation: {
+      type: String as PropType<OrientationType>,
+      default: "center",
+    },
   },
   setup(props, { slots, emit }) {
-    const classList = ["v-vidider", `v-vidider--${props.type}`];
+    const showText = slots.default && props.type !== "vertical";
 
-    return { classList };
+    const classList = [
+      "v-divider",
+      `v-divider--type-${props.type}`,
+      `v-divider--orientation-${props.orientation}`,
+      {
+        "v-divider--with-text": showText,
+        "v-divider--with-text-left": showText && props.orientation === "left",
+        "v-divider--with-text-right": showText && props.orientation === "right",
+      },
+    ];
+
+    return { classList, showText };
   },
 });
 </script>
 
 <style lang="scss">
-.v-vidider {
+.v-divider {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
-  color: $-color--border-base;
+  color: $-color--text-primary;
   font-size: 14px;
   font-variant: tabular-nums;
   line-height: 1.5715;
   list-style: none;
   font-feature-settings: "tnum";
-  border-top: 1px solid $-color--border-extra-light;
 }
 
-.v-divider--horizontal {
+.v-divider--type-horizontal {
   display: flex;
   clear: both;
   width: 100%;
   min-width: 100%;
   margin: 24px 0;
+  border-top: 1px solid $-color--border-extra-light;
 }
 
-.v-divider--vertical {
+.v-divider--type-vertical {
   position: relative;
   top: -0.06em;
   display: inline-block;
@@ -54,5 +75,55 @@ export default defineComponent({
   vertical-align: middle;
   border-top: 0;
   border-left: 1px solid $-color--border-extra-light;
+}
+
+.v-divider--with-text {
+  display: flex;
+  margin: 16px 0;
+  color: $-color--text-primary;
+  font-weight: 500;
+  font-size: 16px;
+  white-space: nowrap;
+  text-align: center;
+  border-top: 0;
+}
+
+.v-divider--with-text::before,
+.v-divider--with-text::after {
+  position: relative;
+  top: 50%;
+  width: 50%;
+  border-top: 1px solid $-color--border-extra-light;
+  transform: translateY(50%);
+  content: "";
+}
+
+.v-divider--orientation-left {
+  &::before {
+    top: 50%;
+    width: 5%;
+  }
+
+  &::after {
+    top: 50%;
+    width: 95%;
+  }
+}
+
+.v-divider--orientation-right {
+  &::before {
+    top: 50%;
+    width: 95%;
+  }
+
+  &::after {
+    top: 50%;
+    width: 5%;
+  }
+}
+
+.v-divider--text {
+  display: inline-block;
+  padding: 0 1em;
 }
 </style>
