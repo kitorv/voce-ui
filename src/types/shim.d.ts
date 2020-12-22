199734482bca3c0d52d13bce7447068f3e921145
kitorv <826526881@qq.com>
2020-12-22 14:06:09 +0800

    类型定义

--- src/types/shim.d.ts
+++ src/types/shim.d.ts
@@ -1,11 +1,11 @@
 declare module "*.vue" {
   import type { DefineComponent } from "vue";
-  const component: DefineComponent;
+  const component: DefineComponent<{}, {}, any>;
   export default component;
 }
 
 declare module "*.md" {
   import type { DefineComponent } from "vue";
-  const component: DefineComponent;
+  const component: DefineComponent<{}, {}, any>;
   export default component;
 }
